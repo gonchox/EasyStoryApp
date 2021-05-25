daf10c0124988c32b8ced992fdf33177ccb16359
gonchox <57024307+gonchox@users.noreply.github.com>
2021-05-25 13:48:48 -0500

    Update EasystoryApplication.java

--- src/main/java/com/dystopiastudios/easystory/EasystoryApplication.java
+++ src/main/java/com/dystopiastudios/easystory/EasystoryApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EasystoryApplication {
+public class EasystoryApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EasystoryApplication.class, args);
